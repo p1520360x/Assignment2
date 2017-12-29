b113b8ba5287e4eef17642aaf7bbebbc23a827cd
Khalil Jussab <p1520360x@my365.dmu.ac.uk>
2017-12-29 19:25:45 +0000

    updated gallery, screen shots, code error checker

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-"# Assignment2" 
+# Assignment2"
+
+#Website Link
+
+W3 Validator
+
+#Navigation Structure
+
+#Screen Designs
+
+#Test Plan
+
+#References
+
+#Animation Used
+##Roll over text
+##Button 
+##Automatic slide show 
+
+#API Used
+##Google fonts
+##Google Maps
--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,20 +1,40 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "100%";  // IE10     
+        elements[i].style.flex = "100%";
+    }
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Two images side by side
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "50%";  // IE10       
+        elements[i].style.flex = "50%";
+    }
+}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+// Four images side by side
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";  // IE10   
+        elements[i].style.flex = "25%";       
+    }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
